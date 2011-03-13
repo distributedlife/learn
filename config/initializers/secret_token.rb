e5485303007d3cbbc607699e14ff838ee2a98ebb
distributedlife <rybo@devbase.(none)>
2011-03-13 18:04:34 +1100

    Created rails skeleton :Ryan

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LearnTesting::Application.config.secret_token = 'f942d08c7bd3c9cf9ea04b13dca3970e51fc6f139ad188eda469e00a67313e48f0f430e247dca2df9b7af33b27d46cfeb9ad67a9b79707d5a63f0fd56f05332f'
