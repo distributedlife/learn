a00b313e79bb60c01de6fb383cd0e28cdbd54da8
distributedlife <rybo@devbase.(none)>
2011-03-17 23:01:54 +1100

    added reset style, css deprecation style and added machinist into specs :Ryan

--- spec/support/learning_objectives_fixtures.rb
+++ spec/support/learning_objectives_fixtures.rb
@@ -1,7 +1,3 @@
 def create_learning_objective(brief, discipline)
-#  @lo = LearningObjective.make(:brief => brief, :discipline => discipline)
-  @lo = LearningObjective.new
-  @lo.brief = brief
-  @lo.discipline = discipline
-  @lo.save
+  @lo = LearningObjective.make(:brief => brief, :discipline => discipline)
 end
