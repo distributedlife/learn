4c6f2c0303e53528785499c792c0facef2f0130a
Ryan Boucher <ryan.boucher@distributedlife.com>
2011-11-05 13:50:43 +1100

    added omniauth configuration

--- spec/controllers/authentications_controller_spec.rb
+++ spec/controllers/authentications_controller_spec.rb
@@ -1,8 +1,4 @@
 require 'spec_helper'
 
 describe AuthenticationsController do
-  describe 'account creation patterns' do
-#    it 'should allow username-password'
-#    it 'should allow a facebook account to be merge'
-  end
 end
