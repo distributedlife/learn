16f06130bbf6bb91fb0a4c2b953f636ecd3b0f5e
Ryan Boucher <ryan.boucher@distributedlife.com>
2011-11-08 21:30:12 +1100

    fixed up tests around browse and search

--- spec/controllers/learnings_controller_spec.rb
+++ spec/controllers/learnings_controller_spec.rb
@@ -191,37 +191,22 @@ describe LearningsController do
     end
 
     it 'should be successful' do
-      get 'search'
+      get :search, :q => "hi"
+
       response.should be_success
     end
 
-    it 'should return all rows when query is not provided' do
+    it 'should redirect to browse when no query supplied' do
       get 'search'
 
-      assigns(:learning_objectives).count.should be 5
+      response.should redirect_to browse_learnings_path
      end
-
-    it 'should return all rows when query is blank' do
-      get(:search, {:q => ''})
-
-      assigns(:learning_objectives).count.should be 5
-    end
 
     it 'should filter results when query is provided' do
       get(:search, {:q => 'dog'})
 
       assigns(:learning_objectives).count.should be 1
       assigns(:learning_objectives).first.brief.should == 'the quick brown fox jumps over the lazy dog'
-    end
-
-    it 'should order unfiltered results alphabetically by discipline then category then brief' do
-      get(:search, {:q => ''})
-
-      assigns(:learning_objectives)[0].brief.should == 'aaaaa bbbbb ccccc ddddd eeeee fffff ggggg'
-      assigns(:learning_objectives)[1].brief.should == 'aba quick brown fox jumps over the lazy zog'
-      assigns(:learning_objectives)[2].brief.should == 'zztop automation sauce'
-      assigns(:learning_objectives)[3].brief.should == 'the quick brown fox jumps over the lazy dog'
-      assigns(:learning_objectives)[4].brief.should == 'aaaaa hhhhh iiiii jjjjj kkkkk lllll mmmmm'
     end
 
     it 'should order filtered results alphabetically by discipline then category then brief' do
@@ -251,6 +236,32 @@ describe LearningsController do
 
       assigns(:learning_objectives).count.should be 1
       assigns(:learning_objectives)[0].brief.should == 'aba quick brown fox jumps over the lazy zog'
+    end
+  end
+
+  describe '"GET" browse' do
+    before do
+      create_learning_objective('the quick brown fox jumps over the lazy dog', 'fundamentals', 'concept')
+      create_learning_objective('aba quick brown fox jumps over the lazy zog', 'automation', 'responsibility')
+      create_learning_objective('aaaaa bbbbb ccccc ddddd eeeee fffff ggggg', 'automation', 'lens')
+      create_learning_objective('aaaaa hhhhh iiiii jjjjj kkkkk lllll mmmmm', 'user interaction', 'artefact')
+      create_learning_objective('zztop automation sauce', 'domain knowledge', 'technique')
+    end
+    
+    it 'should return all learning objectives' do
+      get :browse
+
+      assigns(:learning_objectives).count.should be 5
+    end
+
+    it 'should order unfiltered results alphabetically by discipline then category then brief' do
+      get :browse
+
+      assigns(:learning_objectives)[0].brief.should == 'aaaaa bbbbb ccccc ddddd eeeee fffff ggggg'
+      assigns(:learning_objectives)[1].brief.should == 'aba quick brown fox jumps over the lazy zog'
+      assigns(:learning_objectives)[2].brief.should == 'zztop automation sauce'
+      assigns(:learning_objectives)[3].brief.should == 'the quick brown fox jumps over the lazy dog'
+      assigns(:learning_objectives)[4].brief.should == 'aaaaa hhhhh iiiii jjjjj kkkkk lllll mmmmm'
     end
   end
 
