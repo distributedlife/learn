eab7ef323ffd84c59fed206369c97b4920718a32
Ryan Boucher <ryan.boucher@distributedlife.com>
2011-11-09 00:52:00 +0000

    fixed bork

--- app/controllers/learnings_controller.rb
+++ app/controllers/learnings_controller.rb
@@ -1,4 +1,35 @@
-Objective.find params[:id]
+class LearningsController < ApplicationController
+  before_filter :authenticate_user!, :except => ['show', 'search', 'browse']
+
+  def new
+    @learning = LearningObjective.new
+  end
+
+  def create
+    @learning = LearningObjective.new(params[:learning_objective])
+    render :template => "learnings/new" and return if @learning.invalid?
+
+    @learning.save
+
+    success_redirect_to t('notice.learning-created'), learning_path(@learning.id)
+  end
+
+  def show
+    return error_redirect_to t('notice.not-found'), learnings_path unless LearningObjective::exists? params[:id]
+
+    @learning = LearningObjective.find params[:id]
+
+    if user_signed_in?
+      @assessment = current_user.get_assessment @learning.id
+      @assessment ||= UserAssessments.new(:user_id => current_user.id, :learning_objective_id => @learning.id, :awareness => 'not assessed', :guidance => 'not assessed')
+      @assessment.save
+    end
+  end
+
+  def approve
+    return error_redirect_to t('notice.not-found'), learnings_path unless LearningObjective::exists? params[:id]
+
+    @learning = LearningObjective.find params[:id]
     return error_redirect_to t('notice.not-authorised'), learnings_path unless current_user.admin?
 
     
