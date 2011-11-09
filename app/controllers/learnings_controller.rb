4e1520b93f393b7f736ecf0f7c331c5a618727b5
Ryan Boucher <ryan.boucher@distributedlife.com>
2011-11-09 03:06:36 +0000

    fixed c9 fucking with my code

--- app/controllers/learnings_controller.rb
+++ app/controllers/learnings_controller.rb
@@ -1,4 +1,86 @@
-offset(offset).search(query_to_search_for, @disciplines, @categories, @show_pending)
+class LearningsController < ApplicationController
+  before_filter :authenticate_user!, :except => ['show', 'search', 'browse']
+
+  def new
+    @learning = LearningObjective.new
+  end
+
+  def create
+    @learning = LearningObjective.new(params[:learning_objective])
+    render :template => "learnings/new" and return if @learning.invalid?
+
+    @learning.save
+
+    success_redirect_to t('notice.learning-created'), learning_path(@learning.id)
+  end
+
+  def show
+    return error_redirect_to t('notice.not-found'), learnings_path unless LearningObjective::exists? params[:id]
+
+    @learning = LearningObjective.find params[:id]
+
+    if user_signed_in?
+      @assessment = current_user.get_assessment @learning.id
+      @assessment ||= UserAssessments.new(:user_id => current_user.id, :learning_objective_id => @learning.id, :awareness => 'not assessed', :guidance => 'not assessed')
+      @assessment.save
+    end
+  end
+
+  def approve
+    return error_redirect_to t('notice.not-found'), learnings_path unless LearningObjective::exists? params[:id]
+
+    @learning = LearningObjective.find params[:id]
+    return error_redirect_to t('notice.not-authorised'), learnings_path unless current_user.admin?
+
+    
+    @learning.approve!
+    
+    redirect_to :back
+  end
+
+  def edit
+    return error_redirect_to t('notice.not-found'), learnings_path unless LearningObjective::exists? params[:id]
+
+    @learning = LearningObjective.find params[:id]
+    return error_redirect_to t('notice.not-authorised'), learnings_path unless current_user.admin?
+  end
+
+  def update
+    return error_redirect_to t('notice.not-found'), learnings_path unless LearningObjective::exists? params[:id]
+
+    @learning = LearningObjective.find params[:id]
+    return error_redirect_to t('notice.not-authorised'), learnings_path unless current_user.admin?
+
+    @learning.update_attributes(params[:learning_objective])
+    render :template => "learnings/edit" and return if @learning.invalid?
+
+    @learning.save
+    success_redirect_to t('notice.learning-updated'), learning_path(@learning.id)
+  end
+
+  def search
+    @q = ''
+    @show_pending = true
+
+    @q = params[:q] unless params[:q].nil?
+    redirect_to browse_learnings_path and return if @q.blank?
+
+    @page = params[:page]
+    @page ||= 1
+    @page = @page.to_i
+
+    limit = Rails.application.config.page_size
+    offset = (@page - 1) * limit
+    @nextpage = search_learnings_path(:q => @q, :page => @page + 1)
+    
+    
+    @disciplines = LearningObjective.trim_invalid_disciplines(@q.split(' '))
+    @categories = LearningObjective.trim_invalid_categories(@q.split(' '))
+
+    #don't use search query if it is still within our filter
+    query_to_search_for = adjust_query_if_it_was_a_filter(@q, @disciplines, @categories)
+
+    @learning_objectives = LearningObjective::order(:discipline).order(:category).order(:brief).limit(limit).offset(offset).search(query_to_search_for, @disciplines, @categories, @show_pending)
     @result_count = LearningObjective::order(:discipline).order(:category).order(:brief).search(query_to_search_for, @disciplines, @categories, @show_pending).count
     @pages =  @result_count / limit
     @pages = @pages + 1 if LearningObjective.count % limit != 0
